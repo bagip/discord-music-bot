d0fa220b299a0e2321db7f4992c3f4e2b29b36d0
Blagoje <blagoje.petkovic77@gmail.com>
2022-10-17 01:56:19 +0200

    yes

--- main.py
+++ main.py
@@ -14,14 +14,6 @@ bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 token = "ODg4MjYwNjE5MDQyNzAxMzMy.G8C2DY.ocPgEC8V_KgcleT2g4N4iVhgZdQKyNd-6yVCV4"
 
-@bot.event
-async def on_member_update(before, after):
-        if str(before.status) == "idle":
-            if str(after.status) == "online":
-                if str(after.name) == "Bagi":
-                    channel = bot.get_channel(998012382737600544)
-                    await channel.send("Sasa matic gasic")
-
 async def load():
     await bot.add_cog(help_cog(bot))
     await bot.add_cog(music_cog(bot))
